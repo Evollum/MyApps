b6e24c1f62aa03cc0f4c3258880c58854f1ddf8c
Evollum <elisabeth-tuppa@hotmail.com>
2025-03-23 17:58:40 +0100

    Clean up project and add MainComponent

--- smart-todo-app/src/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const generateUniqueId = (): string => {
-    return `task_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
-};
-
-export const filterCompletedTasks = (tasks: Array<{ completed: boolean }>): Array<{ completed: boolean }> => {
-    return tasks.filter(task => !task.completed);
-};
